c317a672a5f97d13740000c7962cbdbfa4f6d91b
Kenwanjohi <wanjohiken85@gmail.com>
2024-01-23 22:39:35 +0300

    Add React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,17 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/theme";
 import { AuthProvider } from "@/contexts/authentication";
+import { QueryClient, QueryClientProvider } from "react-query";
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <AuthProvider>
-        <Component {...pageProps} />
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Component {...pageProps} />
+        </AuthProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
