6b6c6186528cb8077ce063fe609fbeb84ca2c532
Kenwanjohi <wanjohiken85@gmail.com>
2024-02-04 23:58:19 +0300

    Add toast notifications

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@/theme";
+import { Toaster } from 'sonner';
 import { AuthProvider } from "@/contexts/authentication";
 import { QueryClient, QueryClientProvider } from "react-query";
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
           <Component {...pageProps} />
+          <Toaster/>
         </AuthProvider>
       </QueryClientProvider>
     </ChakraProvider>
